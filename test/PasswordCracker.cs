bfbb1af0a0d4a4fed15467a73ced3f4b471ec7d8
Kaustubh Jaiswal <kaustubh007j@gmail.com>
2024-08-06 22:29:26 +0530

    Practice Session

--- test/PasswordCracker.cs
+++ test/PasswordCracker.cs
@@ -36,20 +36,20 @@ namespace test
 
     public class Program4
     {
-        public static void Main(string[] args)
-        {
-            List<string> passwords1 = new List<string> { "abra", "ka", "dabra" };
-            string loginAttempt1 = "abrakadabra";
-            Console.WriteLine(PasswordCrackers.PasswordCracker(passwords1, loginAttempt1));  // Output: "abra ka dabra"
+        //public static void Main(string[] args)
+        //{
+            //List<string> passwords1 = new List<string> { "abra", "ka", "dabra" };
+            //string loginAttempt1 = "abrakadabra";
+            //Console.WriteLine(PasswordCrackers.PasswordCracker(passwords1, loginAttempt1));  // Output: "abra ka dabra"
 
-            List<string> passwords2 = new List<string> { "ka", "abra" };
-            string loginAttempt2 = "kaabra";
-            Console.WriteLine(PasswordCrackers.PasswordCracker(passwords2, loginAttempt2));  // Output: "ka abra"
+            //List<string> passwords2 = new List<string> { "ka", "abra" };
+            //string loginAttempt2 = "kaabra";
+            //Console.WriteLine(PasswordCrackers.PasswordCracker(passwords2, loginAttempt2));  // Output: "ka abra"
 
-            List<string> passwords3 = new List<string> { "ab", "ba", "abab" };
-            string loginAttempt3 = "aba";
-            Console.WriteLine(PasswordCrackers.PasswordCracker(passwords3, loginAttempt3));  // Output: "WRONG PASSWORD"
-        }
+            //List<string> passwords3 = new List<string> { "ab", "ba", "abab" };
+            //string loginAttempt3 = "aba";
+            //Console.WriteLine(PasswordCrackers.PasswordCracker(passwords3, loginAttempt3));  // Output: "WRONG PASSWORD"
+        //}
     }
 }
 
