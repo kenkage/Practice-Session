98167c7d0e6be438c4f850febd2627aeda100fb7
Kaustubh Jaiswal <kaustubh007j@gmail.com>
2024-09-02 23:56:22 +0530

    Secret Code Problem

--- test/Dynamic Programming/MinCoinsRequired1.cs
+++ test/Dynamic Programming/MinCoinsRequired1.cs
@@ -4,33 +4,33 @@ namespace test
     public class MinCoinsRequired1
     {
 
-        static void Main()
-        {
-            string[] parts = Console.ReadLine().Split(' ');
-            int n = int.Parse(parts[0]);
-            int sum = int.Parse(parts[1]);
-            int[] coins = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        //static void Main()
+        //{
+        //    string[] parts = Console.ReadLine().Split(' ');
+        //    int n = int.Parse(parts[0]);
+        //    int sum = int.Parse(parts[1]);
+        //    int[] coins = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
 
-            //int res = MinimumCoinsRecursion(coins, n, sum);
-            //Console.WriteLine(res < 0 ? -1 : res);
-            if (sum != 0)
-            {
-                int[,] dp = new int[n + 1, sum + 1];
-                for (int i = 0; i < n + 1; i++)
-                {
-                    for (int j = 0; j < sum + 1; j++)
-                    {
-                        dp[i, j] = -1;
-                    }
-                }
-                int resDP = MinimumCoins(coins, 0, sum, 0);
-                Console.WriteLine(resDP == int.MaxValue ? -1 : resDP);
-            }
-            else
-            {
-                Console.WriteLine(0);
-            }
-        }
+        //    //int res = MinimumCoinsRecursion(coins, n, sum);
+        //    //Console.WriteLine(res < 0 ? -1 : res);
+        //    if (sum != 0)
+        //    {
+        //        int[,] dp = new int[n + 1, sum + 1];
+        //        for (int i = 0; i < n + 1; i++)
+        //        {
+        //            for (int j = 0; j < sum + 1; j++)
+        //            {
+        //                dp[i, j] = -1;
+        //            }
+        //        }
+        //        int resDP = MinimumCoins(coins, 0, sum, 0);
+        //        Console.WriteLine(resDP == int.MaxValue ? -1 : resDP);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine(0);
+        //    }
+        //}
 
         static int MinimumCoins(int[] coins, int idx, int sum, int count)
         {
--- test/Dynamic Programming/SecretCode.cs
+++ test/Dynamic Programming/SecretCode.cs
@@ -3,7 +3,30 @@ namespace test.DynamicProgramming
 {
 	public class SecretCode
 	{
-		  
+		//static void Main()
+		//{
+		//	int n = int.Parse(Console.ReadLine());
+		//	int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+		//	int res = Solve(arr, 0, n);
+		//	Console.WriteLine(res);
+		//}
+
+		static int Solve(int[] arr, int i, int n)
+		{
+			if (i == n) return 1;
+
+			if (arr[i] == 0) return 0;
+
+			int pick = Solve(arr, i + 1, n); // for single digit
+
+			if(i + 1 <= n-1 && (arr[i] * 10 + arr[i+1]) <= 26)  // check if the next digit of double digit is not exceeding the length of array: i+1 <= n-1
+            {                                                   // to check if double digit formed will be less than 26
+                pick += Solve(arr, i + 2, n);
+			}
+
+			return pick;
+		}
 	}
 }
 
