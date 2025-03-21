b224b11c1180191bfce59b0f33fc9f64a6dd48b5
Kaustubh Jaiswal <kaustubh007j@gmail.com>
2025-03-22 00:02:28 +0530

    Practice Session

--- test/Binary Trees/ConcatenateNodes.cs
+++ test/Binary Trees/ConcatenateNodes.cs
@@ -1,36 +1,36 @@
 ﻿using System;
 namespace test.BinaryTrees
 {
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
+    //public class TreeNode
+    //{
+    //    public int val;
+    //    public TreeNode left;
+    //    public TreeNode right;
 
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
+    //    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    //    {
+    //        this.val = val;
+    //        this.left = left;
+    //        this.right = right;
+    //    }
+    //}
 
     public class ConcatenateNodes
 	{
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter the values in level-order traversal (-1 for null nodes):");
-            List<int?> values = new List<int?>();
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter the values in level-order traversal (-1 for null nodes):");
+        //    List<int?> values = new List<int?>();
 
-            string[] inputs = Console.ReadLine().Split();
-            foreach (string input in inputs)
-            {
-                values.Add(input == "-1" ? (int?)null : int.Parse(input));
-            }
+        //    string[] inputs = Console.ReadLine().Split();
+        //    foreach (string input in inputs)
+        //    {
+        //        values.Add(input == "-1" ? (int?)null : int.Parse(input));
+        //    }
 
-            TreeNode root = BuildTreeFromInput(values);
-            
-        }
+        //    TreeNode root = BuildTreeFromInput(values);
+        //    Concatenate(root);
+        //}
 
         private static TreeNode BuildTreeFromInput(List<int?> values)
         {
@@ -60,6 +60,26 @@ namespace test.BinaryTrees
                 i++;
             }
             return root;
+        }
+
+        private static void Concatenate(TreeNode root)
+        {
+            if (root == null) return;
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            List<string> nodeValue = new List<string>();
+
+            while(queue.Count > 0)
+            {
+                TreeNode current = queue.Dequeue();
+                nodeValue.Add(current.val.ToString());
+
+                if (current.left != null) queue.Enqueue(current.left);
+                if (current.right != null) queue.Enqueue(current.right);
+            }
+            nodeValue.Sort((a, b) => (b + a).CompareTo(a + b));
+            Console.WriteLine(string.Join("", nodeValue));
         }
     }
 }
