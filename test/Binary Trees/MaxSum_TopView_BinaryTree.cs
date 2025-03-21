d95f0a9e01ec30bba1da62e745ba635050375193
Kaustubh Jaiswal <kaustubh007j@gmail.com>
2025-03-21 21:44:51 +0530

    Prerequisites

--- test/Binary Trees/MaxSum_TopView_BinaryTree.cs
+++ test/Binary Trees/MaxSum_TopView_BinaryTree.cs
@@ -3,37 +3,37 @@ using System.Collections.Generic;
 
 namespace test.BinaryTrees
 {
-	public class TreeNode
-	{
-		public int val;
-		public TreeNode left;
-		public TreeNode right;
+	//public class TreeNode
+	//{
+	//	public int val;
+	//	public TreeNode left;
+	//	public TreeNode right;
 
-		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-		{
-			this.val = val;
-			this.left = left;
-			this.right = right;		
-		}
-	}
+	//	public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+	//	{
+	//		this.val = val;
+	//		this.left = left;
+	//		this.right = right;		
+	//	}
+	//}
 
 	public class MaxSum_TopView_BinaryTree
 	{
-		static void Main(string[] args)
-		{
-            Console.WriteLine("Enter the values in level-order traversal (-1 for null nodes):");
-			List<int?> values = new List<int?>();
+		//static void Main(string[] args)
+		//{
+  //          Console.WriteLine("Enter the values in level-order traversal (-1 for null nodes):");
+		//	List<int?> values = new List<int?>();
 
-			string[] inputs = Console.ReadLine().Split();
-			foreach(string input in inputs)
-			{
-				values.Add(input == "-1" ? (int?)null : int.Parse(input));
-			}
+		//	string[] inputs = Console.ReadLine().Split();
+		//	foreach(string input in inputs)
+		//	{
+		//		values.Add(input == "-1" ? (int?)null : int.Parse(input));
+		//	}
 
-			TreeNode root = BuildTreeFromInput(values);
-			Solution solution = new Solution();
-			Console.WriteLine(solution.MaxSumTopView(root));
-        }
+		//	TreeNode root = BuildTreeFromInput(values);
+		//	Solution solution = new Solution();
+		//	Console.WriteLine(solution.MaxSumTopView(root));
+  //      }
 
         private static TreeNode BuildTreeFromInput(List<int?> values)
         {
