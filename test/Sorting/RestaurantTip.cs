cee13ec0ce0daa09e1b64d2d311825b3abef1461
Kaustubh Jaiswal <kaustubh007j@gmail.com>
2025-03-25 01:53:09 +0530

    Bug Fix - Comment updated

--- test/Sorting/RestaurantTip.cs
+++ test/Sorting/RestaurantTip.cs
@@ -25,30 +25,35 @@ namespace test.Sorting
 
             friends.Sort((a, b) => b.budget.CompareTo(a.budget));
 
-            HashSet<int> used = new HashSet<int>(); // Keep track of used friends
+            //HashSet<int> used = new HashSet<int>(); // Keep track of used friends // this fails in case of duplicate budget/food amount.
+            // 6
+            // 2 3 6 9 5 7
+            // 3 2 7 10 6 10
+            bool[] used = new bool[n]; // Track used friends
 
-            for (int i = 0; i < n - 1; i++)  // At least two people per group
+            for (int i = 0; i < n; i++)
             {
-                if (used.Contains(i)) continue; // Skip used friends
+                if (used[i]) continue; // Skip already used friends
 
                 int totalFood = friends[i].food;
                 int totalBudget = friends[i].budget;
-                int count = 1; // Count of people in the group
+                List<int> group = new List<int> { i }; // Store indices of the group
 
                 for (int j = i + 1; j < n; j++)
                 {
-                    if (used.Contains(j)) continue; // Skip used friends
+                    if (used[j]) continue; // Skip already used
 
                     totalFood += friends[j].food;
                     totalBudget += friends[j].budget;
-                    count++;
+                    group.Add(j);
 
-                    // Check if this group is valid
-                    if (count >= 2 && totalBudget >= totalFood)
+                    // Ensure at least two people in a valid group
+                    if (group.Count >= 2 && totalBudget >= totalFood)
                     {
                         // Mark these friends as used
-                        used.Add(i);
-                        used.Add(j);
+                        foreach (var idx in group)
+                            used[idx] = true;
+
                         days++;
                         break; // Move to the next day
                     }
