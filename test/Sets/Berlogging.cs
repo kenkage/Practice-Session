fdc647890144e2dc99ddfb66d29fa22752d13d43
Kaustubh Jaiswal <kaustubh007j@gmail.com>
2025-03-30 19:31:00 +0530

    Practice Session
    Co-Authored-By: Kaustubh Jaiswal <kaustubh007j@gmail.com>

--- test/Sets/Berlogging.cs
+++ test/Sets/Berlogging.cs
@@ -3,51 +3,51 @@ using System.Collections.Generic;
 
 class BerloggingVictory
 {
-    static void Main(string[] args)
-    {
-        int n = int.Parse(Console.ReadLine());
+    //static void Main(string[] args)
+    //{
+    //    int n = int.Parse(Console.ReadLine());
 
-        // Initialize dictionaries
-        Dictionary<string, int> scores = new Dictionary<string, int>();
-        List<(string name, int round, int cumulativeScore)> scoreHistory = new List<(string, int, int)>();
+    //    // Initialize dictionaries
+    //    Dictionary<string, int> scores = new Dictionary<string, int>();
+    //    List<(string name, int round, int cumulativeScore)> scoreHistory = new List<(string, int, int)>();
 
-        for (int i = 0; i < n; i++)
-        {
-            string[] input = Console.ReadLine().Split();
-            string player = input[0];
-            int scoreChange = int.Parse(input[1]);
+    //    for (int i = 0; i < n; i++)
+    //    {
+    //        string[] input = Console.ReadLine().Split();
+    //        string player = input[0];
+    //        int scoreChange = int.Parse(input[1]);
 
-            // Update cumulative score for the player
-            if (!scores.ContainsKey(player))
-            {
-                scores[player] = 0;
-            }
-            scores[player] += scoreChange;
+    //        // Update cumulative score for the player
+    //        if (!scores.ContainsKey(player))
+    //        {
+    //            scores[player] = 0;
+    //        }
+    //        scores[player] += scoreChange;
 
-            // Record the round and cumulative score for the player
-            scoreHistory.Add((player, i + 1, scores[player]));
-        }
+    //        // Record the round and cumulative score for the player
+    //        scoreHistory.Add((player, i + 1, scores[player]));
+    //    }
 
-        int maxScore = int.MinValue;
-        foreach (var score in scores.Values)
-        {
-            if (score > maxScore)
-            {
-                maxScore = score;
-            }
-        }
+    //    int maxScore = int.MinValue;
+    //    foreach (var score in scores.Values)
+    //    {
+    //        if (score > maxScore)
+    //        {
+    //            maxScore = score;
+    //        }
+    //    }
 
-        // Determine who reached the max score first
-        string winner = null;
-        foreach (var record in scoreHistory)
-        {
-            if (record.cumulativeScore >= maxScore)
-            {
-                winner = record.name;
-                break;
-            }
-        }
+    //    // Determine who reached the max score first
+    //    string winner = null;
+    //    foreach (var record in scoreHistory)
+    //    {
+    //        if (record.cumulativeScore >= maxScore)
+    //        {
+    //            winner = record.name;
+    //            break;
+    //        }
+    //    }
 
-        Console.WriteLine(winner);
-    }
+    //    Console.WriteLine(winner);
+    //}
 }
---
+++ test/Sets/LongestSubarray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace test.Sets
+{
+	public class LongestSubarray
+	{
+        static int FindMaxLength(int[] nums)
+        {
+            Dictionary<int, int> prefixMap = new Dictionary<int, int>();
+            int maxLength = 0, prefixSum = 0;
+
+            // Initialize with prefixSum 0 at index -1
+            prefixMap[0] = -1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // Convert 0 to -1
+                prefixSum += (nums[i] == 0) ? -1 : 1;
+
+                // If the prefixSum has been seen before, update maxLength
+                if (prefixMap.ContainsKey(prefixSum))
+                {
+                    maxLength = Math.Max(maxLength, i - prefixMap[prefixSum]);
+                }
+                else
+                {
+                    // Store the first occurrence of this prefix sum
+                    prefixMap[prefixSum] = i;
+                }
+            }
+            return maxLength;
+        }
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            Console.WriteLine(FindMaxLength(nums));
+        }
+    }
+}
+
