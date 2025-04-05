7beb5d5edcbbfbdd6615998d68df79f96a1acf12
Kaustubh Jaiswal <kaustubh007j@gmail.com>
2025-04-05 20:42:21 +0530

    Create MaxPartitions.cs
    Co-Authored-By: Kaustubh Jaiswal <kaustubh007j@gmail.com>

---
+++ test/Stack/MaxPartitions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace test.Stack
+{
+	public class MaxPartition
+	{
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            Console.WriteLine(MaxPartitions(arr));
+        }
+
+        static int MaxPartitions(int[] arr)
+        {
+            int maxSoFar = 0;
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                maxSoFar = Math.Max(maxSoFar, arr[i]);
+
+                if (maxSoFar == i) // we can make a partition here
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
+
