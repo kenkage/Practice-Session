8e22d7a553d5217e8b1d48289e2bbd5490dec443
Kaustubh Jaiswal <kaustubh007j@gmail.com>
2025-04-12 16:43:00 +0530

    Create BreakTheNumber.cs
    Co-Authored-By: Kaustubh Jaiswal <kaustubh007j@gmail.com>

---
+++ test/Dynamic Programming/BreakTheNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+// ref:https://www.youtube.com/watch?v=BXkK_Ko7uls
+namespace test.DynamicProgramming
+{
+	public class BreakTheNumber
+	{
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(MaxProduct(n));
+        }
+
+        private static int MaxProduct(int n)
+        {
+            if (n == 2) return 1;
+            if (n == 3) return 2;
+            int product = 1;
+
+            while (n > 4)
+            {
+                product = product * 3;
+                n = n - 3;
+            }
+            return product * n;
+        }
+    }
+}
+
