1d2e6c3c9a5a81ada7c1fbd552b3376275ccc7a9
Kaustubh Jaiswal <kaustubh007j@gmail.com>
2025-04-16 22:36:11 +0530

    Update ImplementTrie.cs
    Co-Authored-By: Kaustubh Jaiswal <kaustubh007j@gmail.com>

--- test/Tries/ImplementTrie.cs
+++ test/Tries/ImplementTrie.cs
@@ -1,11 +1,62 @@
 ﻿using System;
+using static test.Tries.ImplementTrie;
+
 namespace test.Tries
 {
-	public class ImplementTrie
+    public class TrieNode
+    {
+        public TrieNode[] Children { get; set; }
+        public bool IsEndOfWord { get; set; }
+
+        public TrieNode()
+        {
+            Children = new TrieNode[26];
+            IsEndOfWord = false;
+        }
+    }
+
+    public class ImplementTrie
 	{
-		public ImplementTrie()
+        private TrieNode root;
+        public ImplementTrie(TrieNode root)
 		{
+            this.root = root;
 		}
+
+        public void Insert(string word)
+        {
+            TrieNode node = root;
+            foreach(char ch in word)
+            {
+                if (node.Children[ch -'a'] == null)
+                {
+                    node.Children[ch - 'a'] = node;
+                }
+                node = node.Children[ch - 'a'];
+            }
+            node.IsEndOfWord = true;
+            Console.WriteLine("Yes");
+        }
 	}
+    public class TrieProgram
+    {
+        //public static void Main()
+        //{
+        //    TrieNode root = new TrieNode();
+
+        //    //Console.WriteLine("Enter the number of words:");
+        //    int n = int.Parse(Console.ReadLine());
+
+        //    //Console.WriteLine("Enter the words:");
+        //    string[] words = Console.ReadLine().Split();
+
+        //    ImplementTrie trie = new ImplementTrie(root);
+
+        //    foreach (var word in words)
+        //    {
+        //        trie.Insert(word);
+        //    }
+        //}
+    }
 }
 
